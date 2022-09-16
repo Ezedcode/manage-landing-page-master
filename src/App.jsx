b4bb655351a534f765e6ef39d083f60556ad1e8d
Ezedélio Garcia <96814597+Ezedcode@users.noreply.github.com>
2022-09-16 16:13:50 +0100

    img

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/layout/header";
+import GlobalStyles from "./styles/global";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <GlobalStyles />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/styles/global.jsx
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Be Vietnam Pro;
+    font-size: 16px;
+  }
+`;
+
+export default GlobalStyles;
